4b9b677f260f5550cf053670cc8dcf0f431702be
Marek Pavelka <marek.pavelka12@gmail.com>
2026-05-07 11:52:40 +0000

    patch1

--- risky.py
+++ risky.py
@@ -6,10 +6,6 @@ import os
 import subprocess
 
 
-def eval_user_input(user_input):
-    # Unsafe: directly evaluating untrusted input
-    return eval(user_input)
-
 
 def command_injection(user_command):
     # Unsafe: shell injection via untrusted input
