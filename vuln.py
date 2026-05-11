ab81ba4d483229fa3558ff91b760684626159ce2
Marek Pavelka <marek.pavelka12@gmail.com>
2026-05-11 13:08:49 +0000

    an

--- vuln.py
+++ vuln.py
@@ -9,3 +9,9 @@ import subprocess
 def eval_user_input(user_input):
     # Unsafe: directly evaluating untrusted input
     return eval(user_input)
+
+
+def path_traversal(filename):
+    # Unsafe: path traversal by concatenating user-controlled values
+    with open(f"/tmp/{filename}", "r") as f:
+        return f.read()
