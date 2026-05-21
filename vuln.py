2cbb463521a3ea57663e39d4e8d82ac0a0aba53c
Marek Pavelka <marek.pavelka12@gmail.com>
2026-05-21 09:01:57 +0000

    mma

--- vuln.py
+++ vuln.py
@@ -5,3 +5,10 @@ HARD_CODED_SECRET = "SuperSecretPassworda123"
 def use_hard_coded_secret():
     # Unsafe: hard-coded credentials / secrets
     return HARD_CODED_SECRET
+
+
+
+def path_traversal(filename):
+    # Unsafe: path traversal by concatenating user-controlled values
+    with open(f"/tmp/{filename}", "r") as f:
+        return f.read()
